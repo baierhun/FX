d04eba7d297bb541f314a0fa6d01f6e3c3b7d9a9
Mr. Baier <paulbaier@hunschool.org>
2025-10-01 13:37:48 -0400

    show generic method

--- code/src/GenericBox.java
+++ code/src/GenericBox.java
@@ -1,3 +1,12 @@
+class ArrayPrinter {
+    public static <V> void printArray(V[] array) {
+        for (V item : array) {
+            System.out.print(item + " ");
+        }
+        System.out.println();
+    }
+}
+
 public class GenericBox<T> {
     private T value;
 
@@ -25,8 +34,17 @@ public class GenericBox<T> {
 
     public static void main(String[] args) {
         GenericBox<String> s = new GenericBox<>("Hello, world!");
+        GenericBox<String> t = new GenericBox<>("How are you?");
+
         GenericBox<Integer> i = new GenericBox<>();
         System.out.println(s);
+
+        String[] fruits = {"apple", "banana", "cherry"};
+        ArrayPrinter.printArray(fruits);
+        Integer[] ages = {1,2,3,4};
+        ArrayPrinter.printArray(ages);
+        // Look into this
+//        GenericBox<String>[] gb = {s, t};
     }
 
 }
